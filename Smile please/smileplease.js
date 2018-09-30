870fb5b10e68b8dc3b1a955bd94579d158d09edd
EKTA WAHI <ekta.mcs18.du@gmail.com>
2018-09-30 13:54:27 +0530

    Add files via upload

---
+++ Smile please/smileplease.js
@@ -0,0 +1,112 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk');
+
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
+  },
+  handle(handlerInput) {
+    const factArr = data;
+    const factIndex = Math.floor(Math.random() * factArr.length);
+    const randomFact = factArr[factIndex];
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .withSimpleCard(SKILL_NAME, randomFact)
+      .getResponse();
+  },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, an error occurred.')
+      .reprompt('Sorry, an error occurred.')
+      .getResponse();
+  },
+};
+
+const SKILL_NAME = 'Smile Please';
+const GET_FACT_MESSAGE = 'Here\'s your joke: ';
+const HELP_MESSAGE = 'You can say tell me a funny joke, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+const data = [
+  'After Monday and Tuesday even the calendar says W T F.',
+  'What did one ocean say to the other ocean? Nothing, they just waved.',
+  'A computer once beat me at chess, but it was no match for me at kick boxing.',
+  'As long as there are tests, there will be prayer in schools.',
+  'You kill vegetarian vampires with a steak to the heart.',
+  'What is faster Hot or cold? Hot, because you can catch a cold.',
+  'If you want to catch a squirrel just climb a tree and act like a nut.',
+  'Not saying I hate you, but if your face was on fire and I had a glass of water I’d drink it.',
+  'My favorite part of a marathon is watching the reaction of runners who grab my plastic cup of vodka.',
+  'A bank is a place that will lend you money, if you can prove that you don’t need it.',
+  'A diplomat is someone who can tell you to go to hell in such a way that you will look forward to the trip.',
+  'Why did the bee get married? Because he found his honey.',
+  'A diplomat is someone who can tell you to go to hell in such a way that you will look forward to the trip.',
+];
+
+const skillBuilder = Alexa.SkillBuilders.standard();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNewFactHandler,
+    HelpHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
