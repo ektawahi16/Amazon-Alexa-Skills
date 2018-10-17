7b89000303999ac5a65fa7274ef9cce679bfaa48
EKTA WAHI <ekta.mcs18.du@gmail.com>
2018-10-17 14:51:08 +0530

    Add files via upload

---
+++ Astro Facts/astro.js
@@ -0,0 +1,114 @@
+/* eslint-disable  func-names */
+/* eslint-disable  no-console */
+
+const Alexa = require('ask-sdk');
+
+const GetNewFactHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'LaunchRequest'
+      || (request.type === 'IntentRequest'
+        && request.intent.name === 'GetNewFactIntent');
+  },
+  handle(handlerInput) {
+    const factArr = data;
+    const factIndex = Math.floor(Math.random() * factArr.length);
+    const randomFact = factArr[factIndex];
+    const speechOutput = GET_FACT_MESSAGE + randomFact;
+
+    return handlerInput.responseBuilder
+      .speak(speechOutput)
+      .withSimpleCard(SKILL_NAME, randomFact)
+      .getResponse();
+  },
+};
+
+const HelpHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && request.intent.name === 'AMAZON.HelpIntent';
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(HELP_MESSAGE)
+      .reprompt(HELP_REPROMPT)
+      .getResponse();
+  },
+};
+
+const ExitHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'IntentRequest'
+      && (request.intent.name === 'AMAZON.CancelIntent'
+        || request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    return handlerInput.responseBuilder
+      .speak(STOP_MESSAGE)
+      .getResponse();
+  },
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    const request = handlerInput.requestEnvelope.request;
+    return request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    console.log(`Session ended with reason: ${handlerInput.requestEnvelope.request.reason}`);
+
+    return handlerInput.responseBuilder.getResponse();
+  },
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, an error occurred.')
+      .reprompt('Sorry, an error occurred.')
+      .getResponse();
+  },
+};
+
+const SKILL_NAME = 'Astro Facts';
+const GET_FACT_MESSAGE = 'Here\'s your fact: ';
+const HELP_MESSAGE = 'You can say tell me a astro fact, or, you can say exit... What can I help you with?';
+const HELP_REPROMPT = 'What can I help you with?';
+const STOP_MESSAGE = 'Goodbye!';
+
+const data = [
+'The Moon May Actually Impact How You Act & Feel',
+'Your Zodiac Sign Is Based On The Sun',
+'31% Of Americans Believe In Astrology',
+'A Number Of Past US Presidents Believed In Astrology',
+'The United Astrology Conference Meets To Predict Things Like Who Will Win The Presidential Race, & They Are Often Correct',
+'Your Astrology Sign Might Influence What You Look Like',
+'Astrology Is Old, Like Really Old',
+'You Might Think You’re One Sign, But Really You’re Another',
+'Researchers Have Found A Connection Between Astrology Signs & Favorite Colors',
+'An Astrology Accurately Predicted Hitler\'s Future',
+'Obama\'s Presidential Race Victory Was Predicted by Astrologers',
+'There Are Water, Fire, Earth, and Air Signs',
+'Your Sign Might Influence Your Appearance',
+'Each Sign Has Favorable Colors',
+'China, India, and Greece Were First Countries to Utilize Astrology',
+];
+
+const skillBuilder = Alexa.SkillBuilders.standard();
+
+exports.handler = skillBuilder
+  .addRequestHandlers(
+    GetNewFactHandler,
+    HelpHandler,
+    ExitHandler,
+    SessionEndedRequestHandler
+  )
+  .addErrorHandlers(ErrorHandler)
+  .lambda();
